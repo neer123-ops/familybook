f61c703a29bc96c976cdf2e56989ad783a2a21d0
neer123-ops <73882872+neer123-ops@users.noreply.github.com>
2021-04-03 14:24:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+image_array = [
+    "..//CODING-IMAGES/father.png",
+    "..//CODING-IMAGES/mother.png",
+    "..//CODING-IMAGES/boy.png"
+];
+ names_array = [
+     "Chakradhar Dubey",
+     "Mitali Dubey",
+     "Neer Dubey"
+ ];
+ next_slide();{
+     document.getElementById("..//CODING-IMAGES/familybook.png").src=image_array[i];
+ }
+ next_slide_name();{
+     document.getElementById("Our Family Member's Names").text=names_array[i];
+ }
